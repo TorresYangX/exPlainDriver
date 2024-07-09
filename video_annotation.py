07408a3fdc1a865ed4ecd65d0c8d66de7631fe60
TorresYangX <12112729@mail.sustech.edu.cn>
2024-07-09 06:17:09 -0700

    video_ann

--- video_annotation.py
+++ video_annotation.py
@@ -1,40 +1,16 @@
 import pandas as pd
-import re
 
-action_map = {
-    ("stop",): "Stop",
-    ("forward",): "Keep",
-    ("accelerate",): "Accelerate",
-    ("brake",): "Decelerate",
-    ("merge",): "Merge",
-    ("right", "turn"): "MakeRightTurn",
-    ("right", "lane"): "ChangeToRightLane",
-    ("left", "turn"): "MakeLeftTurn",
-    ("left", "lane"): "ChangeToLeftLane",
-    ("pull", "over"): "PullOver",
-    ("continue",): "Keep",
-}
+video_indexs = ["06d501fd-a9ffc960.mov", "01b0505f-5f564e84.mov", "06d501fd-fd237e38.mov"]
 
-
-def replace_action(input_str, action_map):
-    input_str_lower = input_str.lower()
-    words = input_str_lower.split()
-    for i in range(len(words)):
-        for keys, value in action_map.items():
-            if all(key in words[i:i+len(keys)] for key in keys):
-                return value
-    return input_str
-
-def query_annotation_csv(path, video_index):
+def query_annotation_csv(path):
+    result_dict = {}
     table = pd.read_csv(path)
     table = table.dropna(subset=['Input.Video'])
-    row = table[table['Input.Video'].str.contains(video_index)].iloc[0].dropna()
-    result_dict = row.to_dict()
+    for video in video_indexs:
+        row = table[table['Input.Video'].str.contains(video)].iloc[0].dropna()
+        row = row.drop('Input.Video')
+        single_dict = row.to_dict()
+        result_dict[video] = single_dict
     return result_dict
-
-    
-if __name__=="__main__":
-    input_str1 = "The car will turn right and then accelerate"
-    result1 = replace_action(input_str1, action_map)
-    print(result1) 
+        
     
