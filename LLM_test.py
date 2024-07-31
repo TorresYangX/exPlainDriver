ec6b64fba3aeba713a6697ee04e34cf53e227f34
TorresYangX <12112729@mail.sustech.edu.cn>
2024-07-31 08:29:48 -0700

    direct compare action

--- LLM_test.py
+++ LLM_test.py
@@ -5,15 +5,15 @@ import numpy as np
 import pandas as pd
 from pgm.PGM import PGM
 from tqdm import tqdm
-from utils import Llama3_map_action, update_action
-from pgm.YOLO_detector import detect_single_frame
-from pgm.predicate_map import segment_to_vector, map_classes_to_vector
+from utils import Llama3_map_action, update_action, LLM_compare_action
+from pgm.predicate_map import segment_to_vector
 
-action_list = ["Keep", "Accelerate", "Decelerate", "Stop", "Reverse", "MakeLeftTurn", "MakeRightTurn", "MakeUTurn", "Merge", "LeftPass", "RightPass", "Yield", "ChangeToLeftLane", "ChangeToRightLane", "ChangeToCenterLeftTurnLane", "Park", "PullOver"]
+action_list = ["Keep", "Accelerate", "Decelerate", "Stop", "Reverse", "MakeLeftTurn", 
+               "MakeRightTurn", "MakeUTurn", "Merge", "LeftPass", "RightPass", "Yield", 
+               "ChangeToLeftLane", "ChangeToRightLane", "Park", "PullOver"]
 
 
 def id2action(data, id):
-    
     for item in data:
         if item["id"] == id:
             return item['action'], item
@@ -21,19 +21,21 @@ def id2action(data, id):
 
 
 
-def LLM_acc(LLM_result_path, ground_truth_info):
+def LLM_acc(LLM_result_path, ground_truth_info, wrong_case_save_path):
     LLM_result = json.load(open(LLM_result_path))
     ground_truth_data = json.load(open(ground_truth_info))
     
-    special_case = []
-    
     total = 0
     correct = 0
+    special = 0
+    wrong_case = []
+    
     for item in tqdm(LLM_result):
         id = item['image_id']
         LLM_action_ = item['caption']
         ground_action_, _ = id2action(ground_truth_data, id)
         if ground_action_ is None:
+            print(f"Ground Truth not found for id: {id}")
             continue
         
         total += 1
@@ -46,27 +48,67 @@ def LLM_acc(LLM_result_path, ground_truth_info):
         LLM_action = Llama3_map_action(LLM_action_)
         ground_action = Llama3_map_action(ground_action_)
         
-        LLM_action = update_action(action_list, LLM_action)
-        ground_action = update_action(action_list, ground_action)
+        LLM_action_set = update_action(action_list, LLM_action)
+        ground_action_set = update_action(action_list, ground_action)
         
-        if LLM_action == ground_action:
+        if LLM_action_set == ground_action_set:
             correct += 1
-        
-        # Check for special cases
-        if (LLM_action, ground_action) in [("Keep", "Accelerate"), ("Accelerate", "Keep"), ("Stop", "Decelerate"), ("Decelerate", "Stop")]:
-            special_case.append({
-                'LLM_origin_action': LLM_action_,
-                'ground_origin_action': ground_action_,
-                'LLM_map_action': LLM_action,
-                'ground_map_action': ground_action
+            print(f"Same -- LLM: {LLM_action_set}, Ground Truth: {ground_action_set}")
+        elif ground_action_set.issubset(LLM_action_set):
+            correct += 1
+            print(f"Include -- LLM: {LLM_action_set}, Ground Truth: {ground_action_set}")
+        elif LLM_action_set.issubset(ground_action_set):
+            special += 1
+            print(f"Special -- LLM: {LLM_action_set}, Ground Truth: {ground_action_set}")
+        else:
+            wrong_case.append({
+                'id': id,
+                'LLM_action': LLM_action_,
+                'ground_action': ground_action_,
+                'LLM_action_set': list(LLM_action_set),
+                'ground_action_set': list(ground_action_set)
             })
-            correct += 1
+            print(f"Wrong -- LLM: {LLM_action_set}, \nGround Truth: {ground_action_set},\n id: {id},\n LLM: {LLM_action_},\n Ground Truth: {ground_action_}")
             
     acc = correct / total
     
-    print(f"Total: {total}, Correct: {correct}, Accuracy: {acc}")
+    print(f"Total: {total}, Correct: {correct}, Special: {special}, Accuracy: {acc}")
     
-    return acc, special_case
+    with open(wrong_case_save_path, 'w') as f:
+        json.dump(wrong_case, f)
+    
+    return acc
+
+
+def LLM_acc_direct(LLM_result_path, ground_truth_info, wrong_case_save_path):
+    LLM_result = json.load(open(LLM_result_path))
+    ground_truth_data = json.load(open(ground_truth_info))
+    total = 0
+    correct = 0
+    wrong_case = []
+    for item in tqdm(LLM_result):
+        id = item['image_id']
+        LLM_action_ = item['caption']
+        ground_action_, _ = id2action(ground_truth_data, id)
+        if ground_action_ is None:
+            print(f"Ground Truth not found for id: {id}")
+            continue
+        LLM_ans = LLM_compare_action(LLM_action_, ground_action_).lower().strip()
+        if 'yes' in LLM_ans:
+            correct += 1
+        else:
+            wrong_case.append({
+                'id': id,
+                'LLM_action': LLM_action_,
+                'ground_action': ground_action_
+            })
+        total += 1
+        
+    acc = correct / total
+    print(f"Total: {total}, Correct: {correct}, Accuracy: {acc}")
+    with open(wrong_case_save_path, 'w') as f:
+        json.dump(wrong_case, f)
+    return acc
 
 
 def LLM_PGM_acc(weight_path, LLM_result_path, ground_truth_info, detection_result, save_redress_path):
@@ -212,14 +254,18 @@ def LLM_PGM_acc(weight_path, LLM_result_path, ground_truth_info, detection_resul
 
 
 if __name__ == "__main__":
-    LLM_result_path = 'Data/video_process/BDDX_Test_pred_action_weight0.0.json'
-    ground_truth_path = 'process_data/test/map_ann_test.json'
     weight_save_path = 'optimal_weights_Interface.npy'
     yolo_detect_Test_path = 'process_data/test/test_detected_classes.json'
-    
     save_redress_path = 'test_case/redress_case_0.0.json'
     
-    LLM_PGM_acc(weight_save_path, LLM_result_path, ground_truth_path, yolo_detect_Test_path, save_redress_path)
+    LLM_result_path = 'Data/video_process/BDDX_Test_pred_action.json'
+    ground_truth_path = 'process_data/test/map_ann_test.json'
+    wrong_case_file = 'test_case/wrong_case.json'
+    acc = LLM_acc_direct(LLM_result_path, ground_truth_path, wrong_case_file)
+    print(acc)
+    
+    
+    # LLM_PGM_acc(weight_save_path, LLM_result_path, ground_truth_path, yolo_detect_Test_path, save_redress_path)
     
     # acc, special_case = LLM_acc(LLM_result_path, ground_truth_path)
     
