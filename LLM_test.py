9255e4a7407f0dfceae4ef1aa7a0ce0765e4389c
TorresYangX <12112729@mail.sustech.edu.cn>
2024-08-10 06:35:08 -0700

    fix PGM acc bugs

--- LLM_test.py
+++ LLM_test.py
@@ -1,10 +1,10 @@
 import os
 import json
-import torch
 import numpy as np
 import pandas as pd
 from pgm.PGM import PGM
 from tqdm import tqdm
+from pgm.config import *
 from utils import Llama3_map_action, update_action, LLM_compare_action
 from pgm.predicate_map import segment_to_vector
 
@@ -111,21 +111,15 @@ def LLM_acc_direct(LLM_result_path, ground_truth_info, wrong_case_save_path):
     return acc
 
 
-def LLM_PGM_acc(weight_path, LLM_result_path, ground_truth_info, detection_result, save_redress_path):
-    pgm = PGM(weight_path=weight_path)
+def LLM_PGM_acc(weight_path, config, LLM_result_path, ground_truth_info, detection_result, wrong_case_save_path):
+    pgm = PGM(weight_path=weight_path, config=config)
     LLM_result = json.load(open(LLM_result_path))
     ground_truth_data = json.load(open(ground_truth_info))
     yolo_detect_Test = json.load(open(detection_result))
     
-    correct_prob = []
-    wrong_prob = []
-    test_num = 100
-
-    if os.path.exists(save_redress_path):
-        os.remove(save_redress_path)
-    
     total = 0
     correct = 0
+    wrong_case = []
     
     i = 0
     for item in tqdm(LLM_result):
@@ -142,11 +136,8 @@ def LLM_PGM_acc(weight_path, LLM_result_path, ground_truth_info, detection_resul
         ground_action_ = ground_action_.lower().strip()
         
         LLM_action = Llama3_map_action(LLM_action_)
-        ground_action = Llama3_map_action(ground_action_)
         LLM_action = update_action(action_list, LLM_action)
-        ground_action = update_action(action_list, ground_action)
-        
-        
+           
         yolo_class = None
         for item in yolo_detect_Test:
             if item['id'] == id:
@@ -160,97 +151,30 @@ def LLM_PGM_acc(weight_path, LLM_result_path, ground_truth_info, detection_resul
         
         instance_vector = np.array(segment_to_vector(instance))
         violate_rule = pgm.validate_instance(instance_vector)
-        instance_prob = pgm.compute_instance_probability(instance_vector)
         if violate_rule:
-            wrong_prob.append(instance_prob)
+            nature_rule_v = config.mapping_natural_rule(violate_rule)
+        
+            #TODO: add violate rule to prompt and regenerate the action
+            # LLM_action_ = model.generate(...)
+            
+        LLM_ans = LLM_compare_action(LLM_action_, ground_action_).lower().strip()
+        if 'yes' in LLM_ans:
+            correct += 1
         else:
-            correct_prob.append(instance_prob)
-        
-        if i == test_num:
-            break
-    
-    correct_prob = np.array(correct_prob)
-    wrong_prob = np.array(wrong_prob)
-    
-    np.save('correct_prob.npy', correct_prob)
-    np.save('wrong_prob.npy', wrong_prob)
-        
-        # if LLM_action_ == ground_action_:
-        #     correct += 1
-        #     continue
-        
-        # LLM_action = Llama3_map(LLM_action_)
-        # ground_action = Llama3_map(ground_action_)
-        
-        # LLM_action = update_action(action_list, LLM_action)
-        # ground_action = update_action(action_list, ground_action)
-        
-        # if LLM_action == ground_action:
-        #     correct += 1
+            wrong_case.append({
+                'id': id,
+                'LLM_action': LLM_action_,
+                'ground_action': ground_action_,
+                'violate_rule': violate_rule
+            })
+        total += 1
+    acc = correct / total
+    print(f"Total: {total}, Correct: {correct}, Accuracy: {acc}")
+    with open(wrong_case_save_path, 'w') as f:
+        json.dump(wrong_case, f)
+    return acc
             
-    #     else:
-    #         yolo_class = None
-    #         for item in yolo_detect_Test:
-    #             if item['id'] == id:
-    #                 yolo_class = item['classes']
-    #                 print(f"YOLO: {yolo_class}, id: {id}")
-    #                 break
-    #         intance = {
-    #             "action": LLM_action,
-    #             "classes": yolo_class
-    #         }    
             
-    #         instance_vector = np.array(segment_to_vector(intance))
-    #         violate_rule = pgm.validate_instance(instance_vector)
-    #         if violate_rule:
-                
-    #             violate_info = {
-    #                 'video_info': video_info,
-    #                 'yolo_class': yolo_class,
-    #                 'LLM_action': LLM_action,
-    #                 'ground_truth_action': ground_action,
-    #                 'violate_rule': violate_rule
-    #             }
-                
-    #             redress_case.append(violate_info)
-
-    #             with open(save_redress_path, 'a') as f:
-    #                 json.dump(violate_info, f)
-    #                 f.write(',\n')
-                    
-    # return correct / total, redress_case
-                
-            
-        
-        # Check for special cases
-    #     elif (LLM_action, ground_action) in [("Keep", "Accelerate"), ("Accelerate", "Keep"), ("Stop", "Decelerate"), ("Decelerate", "Stop")]:
-    #         correct += 1
-            
-    #     else:
-    #         yolo_class = None
-    #         for item in yolo_detect_Test:
-    #             if item['id'] == id:
-    #                 yolo_class = item['classes']
-    #                 print(f"YOLO: {yolo_class}, id: {id}")
-    #                 break
-    #         condition_vector = np.array(map_classes_to_vector(yolo_class))[len(PGM_action_list):]
-    #         _, action_index = pgm.infer_action_probability(condition_vector)
-    #         PGM_LLM_action = PGM_action_list[action_index]
-    #         print(f"LLM: {PGM_LLM_action}, Ground Truth: {ground_action}")
-    #         if PGM_LLM_action == ground_action:
-    #             correct += 1
-    #         elif (PGM_LLM_action, ground_action) in [("Keep", "Accelerate"), ("Accelerate", "Keep"), ("Stop", "Decelerate"), ("Decelerate", "Stop")]:
-    #             correct += 1    
-    #         else:
-    #             continue
-            
-    # acc = correct / total
-    
-    # print(f"Total: {total}, Correct: {correct}, Accuracy: {acc}")
-    
-    # return acc
-    
-
 
 
 if __name__ == "__main__":
